b890ec907899be3bca9ab00c4430930262fa607d
Sebastián Salata R-T <sa.salatart@gmail.com>
2019-11-05 22:55:42 -0300

    feat(organizations): add basic tabs

--- src/locales/es.js
+++ src/locales/es.js
@@ -2,6 +2,9 @@ export default {
   translation: {
     appName: 'HR Attendance',
     nothingHereYet: 'No hay nada aquí aún',
+    attendances: {
+      word: 'asistencias',
+    },
     confirmations: {
       areYouSure: '¿Está seguro/a?',
       cancel: 'Cancelar',
@@ -24,6 +27,7 @@ export default {
     users: {
       email: 'email',
       password: 'contraseña',
+      word: 'usuarios',
     },
     validations: {
       required: 'requerido',
