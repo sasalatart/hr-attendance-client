073c9680e83786ad93a3a8a7ff2423ad66fb908a
Sebastián Salata Ruiz-Tagle <sebastian@salatart.com>
2019-11-05 22:58:27 -0300

    feat(organizations): add basic tabs (#13)

--- src/components/Organizations/Organization/index.jsx
+++ src/components/Organizations/Organization/index.jsx
@@ -1,6 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useRouteMatch } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { AppBar, Tabs, Tab } from '@material-ui/core';
+import {
+  DateRange as DateRangeIcon,
+  PeopleOutline as PeopleOutlineIcon,
+} from '@material-ui/icons';
 import {
   loadOrganization,
   getOrganizationEntity,
@@ -8,15 +14,23 @@ import {
 import { DataPlaceholder, Title } from '../../Common';
 import { useLoadResource } from '../../../hooks';
 
+const tabs = {
+  users: 0,
+  attendances: 1,
+};
+
 export default function Organization() {
   const dispatch = useDispatch();
   const {
     params: { organizationId },
   } = useRouteMatch();
+  const { t } = useTranslation();
+  const [currentTab, setCurrentTab] = useState(tabs.users);
 
   const organization = useSelector(state =>
     getOrganizationEntity(state, { organizationId }),
   );
+
   const loading = useLoadResource(
     useCallback(() => dispatch(loadOrganization(organizationId)), [
       dispatch,
@@ -25,11 +39,31 @@ export default function Organization() {
     !!organization,
   );
 
+  const handleTabChange = useCallback(
+    (event, newTab) => setCurrentTab(newTab),
+    [],
+  );
+
   return (
     <DataPlaceholder loading={loading} resource={organization}>
       {() => (
         <>
           <Title text={organization.name} withBackButton />
+          <AppBar position="static" color="default">
+            <Tabs
+              aria-label={`${organization.name} tabs`}
+              indicatorColor="primary"
+              onChange={handleTabChange}
+              textColor="primary"
+              value={currentTab}
+              centered
+            >
+              <Tab label={t('users.word')} icon={<PeopleOutlineIcon />} />
+              <Tab label={t('attendances.word')} icon={<DateRangeIcon />} />
+            </Tabs>
+          </AppBar>
+          {currentTab === tabs.users && t('users.word')}
+          {currentTab === tabs.attendances && t('attendances.word')}
         </>
       )}
     </DataPlaceholder>
