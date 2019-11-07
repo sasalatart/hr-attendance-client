71deba27057b9efffd70433ba266a5aa491271a8
Sebastián Salata Ruiz-Tagle <sebastian@salatart.com>
2019-11-06 22:49:25 -0300

    feat: display api validation error messages without additional parsing (#17)

--- src/store/middleware/http-error.js
+++ src/store/middleware/http-error.js
@@ -4,12 +4,6 @@ import i18n from '../../locales';
 import { PROMISE_TYPES_SUFFIXES } from '../ducks/common';
 import { logOut, getIsLoggedIn } from '../ducks/sessions';
 
-// TODO: Create a parser for API validation errors
-// eslint-disable-next-line no-unused-vars
-function validationDescription(errors) {
-  return i18n.t('errors.validationPlaceholder');
-}
-
 export default store => next => action => {
   if (
     !action.type.includes(PROMISE_TYPES_SUFFIXES.REJECTED) ||
@@ -18,14 +12,13 @@ export default store => next => action => {
     return next(action);
   }
 
-  const { status, error, errors } = action.payload;
+  const { status, error } = action.payload;
   if (status === 401) {
     const loggedIn = getIsLoggedIn(store.getState());
     if (loggedIn) store.dispatch(logOut());
-
     toastr.warning(loggedIn ? i18n.t('errors.tokenExpired') : error);
   } else {
-    toastr.error(status === 422 ? validationDescription(errors) : error);
+    toastr.error(error);
   }
 
   return next(action);
